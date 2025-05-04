56f6b6d02a437c00904e6cbe352b22bcd2aa0156
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-04 16:55:27 -0500

    Update clase_producto.py

---
+++ clase_producto.py
@@ -0,0 +1,7 @@
+class Producto:
+    def __init__(self, id, nombre, precio, stock, categoria):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+        self.categoria = categoria
