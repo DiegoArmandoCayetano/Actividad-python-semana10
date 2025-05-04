8d42b8e00252f09a89b7fc686a2fa98a2d3f9f28
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-04 16:54:53 -0500

    Update clase_cliente.py

--- clase_cliente.py
+++ clase_cliente.py
@@ -1,6 +1,8 @@
-
-
-
-
-
+class Cliente:
+    def __init__(self, id, nombre, apellidos, email, historial_compras):
+        self.id = id
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.email = email
+        self.historial_compras = historial_compras
 
