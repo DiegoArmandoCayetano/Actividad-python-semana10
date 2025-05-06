a1e05dd2f6abad5a41ef6b89439a2d8138a6a9ab
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-05 21:37:41 -0500

    Add files via upload

---
+++ controlador/conexion.py
@@ -0,0 +1,11 @@
+class Invernadero:
+    def __init__(self, nombre, superficie, tipo_cultivo, fecha_creacion, responsableInvernadero, capacidadInvernadero, sistemaRiego):
+        self.nombre = nombre
+        self.superficie = superficie
+        self.tipo_cultivo = tipo_cultivo
+        self.fecha_creacion = fecha_creacion
+        self.responsableInvernadero = responsableInvernadero
+        self.capacidadInvernadero = capacidadInvernadero
+        self.sistemaRiego = sistemaRiego
+        
+        
