6a072660f2a43bb0434ce9c2d627fd9eeb778666
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-05 20:14:06 -0500

    Delete clase_producto.py

--- clase_producto.py
+++
@@ -1,7 +0,0 @@
-class Producto:
-    def __init__(self, id, nombre, precio, stock, categoria):
-        self.id = id
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
-        self.categoria = categoria
