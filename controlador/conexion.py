9e95e882a1c66d4d95736c6c8595abf10b3339ff
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-07 14:57:50 -0500

    Update conexion.py

--- controlador/conexion.py
+++ controlador/conexion.py
@@ -1,11 +1,3 @@
-class Invernadero:
-    def __init__(self, nombre, superficie, tipo_cultivo, fecha_creacion, responsableInvernadero, capacidadInvernadero, sistemaRiego):
-        self.nombre = nombre
-        self.superficie = superficie
-        self.tipo_cultivo = tipo_cultivo
-        self.fecha_creacion = fecha_creacion
-        self.responsableInvernadero = responsableInvernadero
-        self.capacidadInvernadero = capacidadInvernadero
-        self.sistemaRiego = sistemaRiego
-        
+class Conexion:
+    pass
         
