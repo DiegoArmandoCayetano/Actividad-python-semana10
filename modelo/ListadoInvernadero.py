f9191ee4ae92759a151ed35f6ebb2017ad1d5209
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-07 15:58:58 -0500

    Create ListadoInvernadero.py

---
+++ modelo/ListadoInvernadero.py
@@ -0,0 +1,13 @@
+from modelo.Invernadero import Invernadero
+class NERY:
+    pass
+
+    def __init__(self):
+        self=listaInvernadero= []
+
+    def guardarInvernadero(self,objInvernadero):
+        self.listaInvernadero.append(objInvernadero)
+
+    def eliminarInvernadero(self,dato):
+        self.listaInvernadero.remove(dato)
+    #
