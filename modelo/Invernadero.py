2454d1ee393d36d8691a4c45590d88ca6e7d9d59
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-07 14:58:36 -0500

    Update Invernadero.py

--- modelo/Invernadero.py
+++ modelo/Invernadero.py
@@ -1,8 +1,13 @@
 class Invernadero:
 
-    def __init__(self):
-       self.nombre=""
-       self.apellido=""
+    def __init__(self, nombre, superficie, tipo_cultivo, fecha_creacion, responsableInvernadero, capacidadInvernadero, sistemaRiego):
+        self.nombre = nombre
+        self.superficie = superficie
+        self.tipo_cultivo = tipo_cultivo
+        self.fecha_creacion = fecha_creacion
+        self.responsableInvernadero = responsableInvernadero
+        self.capacidadInvernadero = capacidadInvernadero
+        self.sistemaRiego = sistemaRiego
 
     def getNombre(self):
         return self.nombre
@@ -10,9 +15,7 @@ class Invernadero:
     def setNombre(self, datoNombre):
         self.nombre= datoNombre
 
-    def guardar_cliente(self,datoNombre):
-        print("cliente guardado desde el modelo")
-        print("se ejecuta los SQL")
-        print(datoNombre)
+    def guardar_Invernadero(self,datoNombre):
+        pass
 
 
