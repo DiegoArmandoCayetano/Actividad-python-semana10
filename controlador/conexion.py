f990ea8644ccedfd2fd264143dc413135954ccae
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-07 15:57:49 -0500

    Update conexion.py

--- controlador/conexion.py
+++ controlador/conexion.py
@@ -1,3 +1,12 @@
+from modelo.Invernadero import Invernadero
+from vista.VentanaCuestionario import  VentanaCuestionario
+o
 class Conexion:
-    pass
+    def __init__(self):
+        self.nuevoInvernadero="+"
+
+    def registrarInventario(self,datoInvernadero):
+        self.nuevoInvernadero=Invernadero()
+        self.nuevoInvernadero.setNombre(datoInvernadero[0])
+        self.objInvernadero.agregarInvernadero(nuevoInvernadero)
         
