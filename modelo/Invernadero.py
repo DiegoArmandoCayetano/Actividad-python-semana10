ab85a38352e2cacbca9276711fe7b75071f6fcf3
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-07 15:58:06 -0500

    Update Invernadero.py

--- modelo/Invernadero.py
+++ modelo/Invernadero.py
@@ -1,13 +1,15 @@
+from modelo.ListadoInvernadero import NERY
 class Invernadero:
 
-    def __init__(self, nombre, superficie, tipo_cultivo, fecha_creacion, responsableInvernadero, capacidadInvernadero, sistemaRiego):
-        self.nombre = nombre
+    def __init__(self, entry_nombre, superficie, tipo_cultivo, fecha_creacion, responsableInvernadero, capacidadInvernadero, sistemaRiego):
+        self.nombre = entry_nombre
         self.superficie = superficie
         self.tipo_cultivo = tipo_cultivo
         self.fecha_creacion = fecha_creacion
         self.responsableInvernadero = responsableInvernadero
         self.capacidadInvernadero = capacidadInvernadero
         self.sistemaRiego = sistemaRiego
+        self.objLista=NERY()
 
     def getNombre(self):
         return self.nombre
@@ -15,7 +17,7 @@ class Invernadero:
     def setNombre(self, datoNombre):
         self.nombre= datoNombre
 
-    def guardar_Invernadero(self,datoNombre):
-        pass
+    def guardarInvernadero(self, ListadoInvernadero):
+        self.objLista.guardarInvernadero(ListadoInvernadero)
 
 
