cf0dca0a54fc5dbd25b42f2a76cf9f2c02998d73
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-14 15:07:34 -0500

    Update crear_tablas.py

--- modelo/crear_tablas.py
+++ modelo/crear_tablas.py
@@ -6,10 +6,14 @@ def crear_tablas():
     cursor = con.cursor()
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS usuarios (
+        CREATE TABLE IF NOT EXISTS invernaderos (
             id INT AUTO_INCREMENT PRIMARY KEY,
             nombre VARCHAR(100) NOT NULL,
-            edad INT NOT NULL
+            capacidad float NOT NULL,
+            superficie float not null,
+            tipo varchar(30) not null,
+            responsable varchar(30) not null,
+            sistema_riego varchar (30) not null
         )
     ''')
 
