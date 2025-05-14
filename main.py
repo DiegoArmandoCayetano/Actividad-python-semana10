0ae474f6d36cd538ff64cd34890013c96a12bd7d
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-14 14:57:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# main.py
+from modelo.Conexion import crear_tablas
 import tkinter as tk
 from vista.VentanaInicio import VentanaInicio
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = VentanaInicio(root)
+    crear_tablas()
     root.mainloop()
