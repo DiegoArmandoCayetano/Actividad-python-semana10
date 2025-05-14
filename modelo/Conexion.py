808949e79a1873e0e4ecb4e060d0ef7e07337202
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-14 14:55:32 -0500

    Create Conexion.py

---
+++ modelo/Conexion.py
@@ -0,0 +1,11 @@
+# modelo/conexion.py
+import mysql.connector
+
+def conectar():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",  # Si tienes contraseña en MySQL, ponla aquí
+        database="mi_proyecto_db"
+    )
+    return conexion
