caae282b853f3988f3d1e1fa26fd65ec986aa6f4
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-05-19 01:52:51 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from modelo.crear_tablas import crear_tablas
-import tkinter as tk
-from vista.VentanaInicio import VentanaInicio
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = VentanaInicio(root)
-    crear_tablas()
-    root.mainloop()
